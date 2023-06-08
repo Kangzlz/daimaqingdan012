7d27dde5703a93eb76295ee3cec63f148ad14b22
Kangzlz <127727428+Kangzlz@users.noreply.github.com>
2023-06-08 23:37:13 +0800

    12-2

---
+++ daima12-8.c
@@ -0,0 +1,23 @@
+/*计算两点之间的距离*/
+#include<stdio.h>
+#include<math.h>
+#define sqr(n) ((n)*(n))
+typedef struct{
+    double x;
+    double y;
+}Point;
+double distance_of(Point pa,Point pb){
+    return sqrt(sqr(pa.x-pb.x)+sqr(pa.y-pb.y));
+}
+int main(void){
+    Point crnt,dest;
+
+    printf("当前地点的x坐标:"); scanf("%lf",&crnt.x);
+    printf("         y坐标:"); scanf("%lf",&crnt.y);
+    printf("目的地的x坐标:"); scanf("%lf",&dest.x);
+    printf("        y坐标:"); scanf("%lf",&dest.y);
+
+    printf("到目的地的距离为%.2f。\n",distance_of(crnt,dest));
+
+    return 0;
+}
